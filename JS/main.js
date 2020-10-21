8fe0db510db1aa5faadbb771200107d6f591e5cf
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2020-10-21 16:24:51 -0300

    Link, Var, Alert, Console.log

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var nome = "Vander Sandino";
+var idade = 29;
+var idade2 = 18;
+var frase = "Sport, Uma razão para viver.";
+//alert("Meu nome é " + nome + " e tenho " + idade + " anos.");
+//alert(idade+idade2);
+console.log(nome)
+console.log(idade)
+//console.log(frase.replace("Sport","Íbis"))
+//alert(frase.replace("Sport","íbis"))
+console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+console.log(idade + idade2);
