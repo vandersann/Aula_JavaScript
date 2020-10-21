e8df1127cafb37d2a913dfabf858049c664b462f
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2020-10-21 16:50:46 -0300

    array

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,31 @@
-var nome = "Vander Sandino";
-var idade = 29;
-var idade2 = 18;
-var frase = "Sport, Uma razão para viver.";
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+
+
+// lista = ["Banana","Laranja","Morango"];
+//lista.push = ("Romã");
+//lista.pop =()
+//console.log (lista);
+//console.log(lista[3]);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
+
+//var nome = "Vander Sandino";
+//var idade = 29;
+//var idade2 = 18;
+//var frase = "Sport, Uma razão para viver.";
 //alert("Meu nome é " + nome + " e tenho " + idade + " anos.");
 //alert(idade+idade2);
-console.log(nome)
-console.log(idade)
+
+//console.log(nome)
+//console.log(idade)
+
 //console.log(frase.replace("Sport","Íbis"))
 //alert(frase.replace("Sport","íbis"))
-console.log(frase.toUpperCase());
+//console.log(frase.toUpperCase());
 //console.log(frase.toLowerCase());
-console.log(idade + idade2);
+//console.log(idade + idade2);
